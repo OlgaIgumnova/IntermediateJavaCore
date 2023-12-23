e3f168c96072be07d3a450438c0fb637c6540521
Olga <o2igumn@gmail.com>
2023-12-23 22:34:04 +0900

    add new class

--- src/App.java
+++ src/App.java
@@ -6,16 +6,16 @@ public class App {
     public static void buttonClick() {
         ArrayList<Student> students = new ArrayList<>();
 
-        students.add(new Student("Иван", new ArrayList<>(Arrays.asList(4.0, 5.0, 4.0, 5.0, 5.0, 5.0)), "Информатика"));
-        students.add(new Student("Эльвира", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 5.0, 5.0, 5.0 , 5.0)), "Информатика"));
-        students.add(new Student("Антон", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 5.0, 5.0)), "Математика"));
-        students.add(new Student("Алексей", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 5.0, 4.0)), "Физика"));
-        students.add(new Student("Ирина", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Физика"));
-        students.add(new Student("Григорий", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
-        students.add(new Student("Сергей", new ArrayList<>(Arrays.asList(3.0, 3.0, 3.0, 4.0, 5.0)), "Информатика"));
-        students.add(new Student("Анатолий", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 4.0, 5.0, 5.0)), "Информатика"));
-        students.add(new Student("Семён", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
-        students.add(new Student("Константин", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
+        students.add(new Student("Николай", new ArrayList<>(Arrays.asList(4.0, 5.0, 4.0, 5.0, 5.0, 5.0)), "Информатика"));
+        students.add(new Student("Антон", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 5.0, 5.0, 5.0 , 5.0)), "Информатика"));
+        students.add(new Student("Надежда", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 5.0, 5.0)), "Математика"));
+        students.add(new Student("Вероника", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 5.0, 4.0)), "Физика"));
+        students.add(new Student("Линда", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Физика"));
+        students.add(new Student("Елизавета", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
+        students.add(new Student("Артем", new ArrayList<>(Arrays.asList(3.0, 3.0, 3.0, 4.0, 5.0)), "Информатика"));
+        students.add(new Student("Анжелика", new ArrayList<>(Arrays.asList(4.0, 5.0, 5.0, 4.0, 5.0, 5.0)), "Информатика"));
+        students.add(new Student("Ольга", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
+        students.add(new Student("Вениамин", new ArrayList<>(Arrays.asList(5.0, 5.0, 5.0, 4.0, 5.0)), "Информатика"));
 
         FilterStudents.filterInformatics(students)
                 .forEach(System.out::println);
